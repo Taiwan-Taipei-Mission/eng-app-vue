fab7be946b3503c6c8cd19128eb066ac9e69f974
Robert Hinckley <theroberthinckley@gmail.com>
2018-04-11 16:58:18 +0800

    First version of the app (basic framwework)

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="sideNav"fixed>
+    <v-list>
+      <v-list-tile v-for="item in menuItems" :key="item.title">
+        <v-list-tile-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-action></v-list-tile-action>
+        <v-list-tile-content>{{ item. title }}</v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+    </v-navigation-drawer>
+      <v-toolbar dark class="primary">
+        <v-toolbar-side-icon @click="sideNav =!sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title>Welcome</v-toolbar-title>
+              <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+               <v-btn flat v-for="item in menuItems" :key="item.title">
+          <v-icon left dark>{{ item.icon }}</v-icon>
+                 {{ item.title }}
+               </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main>
+      <router-view></router-view>
+    </main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      sideNav: false,
+      menuItems: [
+        { icon: 'home', title: 'My Class' },
+        { icon: 'location_on', title: 'Find My Class' },
+        { icon: 'settings', title: 'Settings' },
+        { icon: 'help', title: 'Help' },
+        { icon: 'face', title: 'Sign up' },
+        { icon: 'lock_open', title: 'Sign in' }
+      ]
+    }
+  },
+  name: 'App'
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Home from '@/components/Home' /* Todo Create Meetup.vue */
+import Meetups from '@/components/Meetup/Meetups'
+import Profile from '@/components/User/Profile'
+import Signup from '@/components/User/Signup'
+import Signin from '@/components/User/Signin'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/meetups',
+      name: 'Meetup',
+      component: Meetups
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
+    },
+    {
+      path: '/signin',
+      name: 'Signin',
+      component: Signin
+    }
+  ]
+})
